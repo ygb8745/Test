cb9e7f497570342ca7ebcb28c4197fe3bda787d1
YGB8745 <ygb8745@gmail.com>
2023-10-09 17:22:28 +0800

    to_matrix

--- draft/expect_avg.cpp
+++ draft/expect_avg.cpp
@@ -13,17 +13,31 @@ using namespace std;
 int main(){
     // vector<int> v={0,1,2,3,4,5,6};
     int n=10;
+    vector<vector<int>> profit_matrix(1);
+
     for(int i=1; i<= n; i++){
+        profit_matrix.push_back(vector<int>(n+1));
         // cout << i <<"\t"<< (i)/2 << endl;
         int t= (i-1)/2;
         // cout << t << endl;
-        cout << i << "\t" << "-1\t";
+        // cout << i << "\t" << "-1\t";
+        profit_matrix[i][1] = -1;
         for(int j=2;j<=n;j++){
             if((j)/2<=t){
-                cout << -(j/2*2+1) << "\t";
+                // cout << -(j/2*2+1) << "\t";
+                profit_matrix[i][j] = -(j/2*2+1);
             }else{
-                cout << (2*t+1) << "\t";
+                // cout << (2*t+1) << "\t";
+                profit_matrix[i][j] = 2*t+1;
             }
+        }
+        // cout << endl;
+    }
+    
+    for(int i=1; i<= n; i++){
+        cout << i << "\t";
+        for(int j=1;j<=n;j++){
+            cout << profit_matrix[i][j] << "\t";
         }
         cout << endl;
     }
