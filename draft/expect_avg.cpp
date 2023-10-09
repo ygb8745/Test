22a2aec3da41fe42503d4b9e36192fd3092540ee
YGB8745 <ygb8745@gmail.com>
2023-10-09 17:08:35 +0800

    first_ver

--- draft/expect_avg.cpp
+++ draft/expect_avg.cpp
@@ -2,6 +2,29 @@
 0
 1
 2
-3
-4
+..
+N  : (N) // 2
+N+1
 */
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    // vector<int> v={0,1,2,3,4,5,6};
+    int n=10;
+    for(int i=1; i<= n; i++){
+        // cout << i <<"\t"<< (i)/2 << endl;
+        int t= (i-1)/2;
+        // cout << t << endl;
+        cout << i << "\t" << "-1\t";
+        for(int j=2;j<=n;j++){
+            if((j)/2<=t){
+                cout << -(j/2*2+1) << "\t";
+            }else{
+                cout << (2*t+1) << "\t";
+            }
+        }
+        cout << endl;
+    }
+}
